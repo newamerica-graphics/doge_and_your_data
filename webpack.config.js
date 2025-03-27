b38943463425e7d1707e542477422651238c911b
stephen-darling <darling@newamerica.org>
2025-03-27 14:47:04 -0400

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -3,70 +3,68 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 
-module.exports = env => {
-  return {
-    entry: ["./src/index.js"],
-    output: {
-      path: path.join(__dirname, "public"),
-      filename: `bundle.${env.deploy ? "[contenthash]." : ""}js`
-    },
-    externals: {
-      newamericadotorg: "newamericadotorg"
-    },
-    plugins: [
-      env.deploy === "development" && new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebpackPlugin({
-        title: "",
-        chartIDs: [
-          'viz__id'
-        ],
-        inject: false,
-        template: path.resolve(__dirname, "src/index.html")
-      }),
-      env.deploy &&
-        new CompressionPlugin({
-          test: /\.(js|css)$/,
-          filename: "[file].gz[query]"
-        })
-    ].filter(plugin => plugin),
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/env", "@babel/preset-react"],
-              plugins: [
-                "@babel/plugin-proposal-class-properties",
-                "@babel/plugin-proposal-object-rest-spread"
-              ]
-            }
-          }
+module.exports = (env) => {
+    return {
+        entry: ["./src/index.js"],
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: `bundle.${env.deploy ? "[contenthash]." : ""}js`,
         },
-        {
-          test: /\.s?css/,
-          use: [
-            "style-loader",
-            "css-loader",
-            {
-              loader: "postcss-loader",
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    "autoprefixer",
-                    require('cssnano')({
-                      preset: 'default'
-                    })
-                  ]
-                }
-              }
-            },
-            "sass-loader"
-          ]
-        }
-      ]
-    }
-  };
+        externals: {
+            newamericadotorg: "newamericadotorg",
+        },
+        plugins: [
+            env.deploy === "development" && new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                title: "",
+                chartIDs: ["viz"],
+                inject: false,
+                template: path.resolve(__dirname, "src/index.html"),
+            }),
+            env.deploy &&
+                new CompressionPlugin({
+                    test: /\.(js|css)$/,
+                    filename: "[file].gz[query]",
+                }),
+        ].filter((plugin) => plugin),
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/env", "@babel/preset-react"],
+                            plugins: [
+                                "@babel/plugin-proposal-class-properties",
+                                "@babel/plugin-proposal-object-rest-spread",
+                            ],
+                        },
+                    },
+                },
+                {
+                    test: /\.s?css/,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [
+                                        "autoprefixer",
+                                        require("cssnano")({
+                                            preset: "default",
+                                        }),
+                                    ],
+                                },
+                            },
+                        },
+                        "sass-loader",
+                    ],
+                },
+            ],
+        },
+    };
 };
